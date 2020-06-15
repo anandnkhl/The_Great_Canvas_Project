855cf117dd63fc6cbe71d03723b104a73843396b
Nikhil Anand <anandnikhil91@gmail.com>
2020-06-16 01:01:26 +0530

    server initial setup

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var opn = require('opn');
+
+app.get('/', (req, res) => res.sendFile( __dirname + '/index.html'));
+app.get('/Fractal-Tree', (req, res) => res.sendFile( __dirname + '/FractalTree/fractalTree.html'));
+
+app.listen(8080, function(req, res) {
+    console.log('Server running at http://127.0.0.1:8080/');
+    opn('http://127.0.0.1:8080/'); //Auto launch default browser with our home page
+});
