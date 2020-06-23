3f49ca291f8955c361a23f916a573f00b8a351d2
Nikhil Anand <anandnikhil91@gmail.com>
2020-06-23 14:15:11 +0530

    added new trees in fractal tree

--- FractalTree/fractalTree.js
+++ FractalTree/fractalTree.js
@@ -1,11 +1,11 @@
 const canvas = document.getElementById("fractalTreeCanvas");
 // canvas.setAttribute("style","background:#404040; width: 1200; height: 640");
-canvas.width = window.innerWidth - 10;
-canvas.height = window.innerHeight - 10;
+canvas.width = window.innerWidth - 4;
+canvas.height = window.innerHeight - 4;
 
 const ctx = canvas.getContext("2d");
-    
-function draw(startX, startY, len, angle) {
+
+function drawTree(startX, startY, len, thickness, angle) {
   ctx.beginPath();
   ctx.save();
   
@@ -13,19 +13,20 @@ function draw(startX, startY, len, angle) {
   ctx.rotate(angle * Math.PI/180);
   ctx.moveTo(0, 0);
   ctx.lineTo(0, -len);
-  ctx.strokeStyle = "#009900";
+  ctx.strokeStyle = "#669900";
+  ctx.lineWidth = thickness;
   ctx.stroke();
   
   if(len < 5) {
     ctx.restore();
     return;
   }
-  
-      draw(0, -len, len*0.8, -15);
-      draw(0, -len, len*0.8, 15);
-  
-  
+
+  drawTree(0, -len, len*0.8, thickness*0.7, -25); //Left Branches
+  drawTree(0, -len, len*0.8, thickness*0.7, 17);  //Right Branches
   ctx.restore();
 }
 
-draw(120,120,10,0);
+drawTree(innerWidth/6, innerHeight - 4,100, 10, 20);  //Left Tree
+drawTree(innerWidth/2, innerHeight - 4,50, 4, 0);     //Middle Tree
+drawTree(5*innerWidth/6, innerHeight - 4,110, 15, -5); //Right Tree
