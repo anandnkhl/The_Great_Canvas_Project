042c74278b2393e1661417014f6874be00aa0eb5
Nikhil Anand <anandnikhil91@gmail.com>
2020-06-29 01:44:34 +0530

    fixed rebouncing error

--- BounceGravity/bounceGravity.js
+++ BounceGravity/bounceGravity.js
@@ -3,47 +3,36 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 
-const noOfCircles = 2;
+const noOfCircles = 10;
 const radius = innerWidth/30;
 const circles = [];
 
 function Circle (x, y, color){
-    let maxHeight = y;
-    let currentHeight = 0;
-    let gravity = 0.5;
-    let time = 0;
-    this.drawCircle = (centerY) => {
+    let speedY = 0;
+    let gravity = 0.5; //increase this to make falling faster
+    let elasticity = 0.9; // decrease this to lose more energy on hitting ground [0 to 1]
+    
+    this.moveCircle = () => {
+        if (y + radius >= innerHeight && speedY >= -gravity){
+            speedY = -1 * speedY * elasticity;
+        }else{
+            speedY += gravity;
+        }   
+        y += speedY;
+        this.drawCircle();
+    }
+    this.drawCircle = () => {
         ctx.beginPath();
-        ctx.arc(x, centerY, radius, 0, 2*Math.PI);
+        ctx.arc(x, y, radius, 0, 2*Math.PI);
         ctx.strokeStyle = color;
         ctx.lineWidth = 3;
         ctx.stroke();
-    }
-    this.moveCircle = () => {
-        if (currentHeight <= innerHeight - radius ){
-            let speedY = 0.5*(gravity)*(time)*(time);
-            currentHeight =  maxHeight + speedY;
-            this.drawCircle(currentHeight);
-        }else{
-            let speedY = 0.5*(gravity)*(time)*(time);
-            currentHeight =  maxHeight - speedY;
-            this.drawCircle(currentHeight);
-        }
-        if(currentHeight >= innerHeight - radius || currentHeight <= maxHeight){
-            time = 0;
-            if(maxHeight < innerHeight - radius - 2){
-                maxHeight += 10;
-            }else{
-                maxHeight = innerHeight - radius;
-            }
-        }
-        time++; 
     }
 }
 
 for(let i = 0; i < noOfCircles; i++){
     let centerX = radius + Math.random()*(innerWidth - 2*radius);
-    let centerY = Math.random()*(innerHeight/2);
+    let centerY = Math.random()*(innerHeight/2) + radius;
     let randomColor = 'hsl('+ (Math.random()*360) + ', 100%, 50%)';
     
     circles.push( new Circle( centerX, centerY, randomColor))
