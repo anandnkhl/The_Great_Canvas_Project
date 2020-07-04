fbd52b4a74073a388ea360e0c059208fea9c53f9
Nikhil-KNOLSKAPE <anandnikhil91@gmail.com>
2020-07-04 22:03:26 +0530

    added falling effect to stream

--- DigitalRain/digitalRain.js
+++ DigitalRain/digitalRain.js
@@ -3,7 +3,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 
-const noOfStreams = 100;
+const noOfStreams = 150;
 const trailLen = 10;
 const streams = [];
 
@@ -13,6 +13,7 @@ ctx.textAlign = "center";
  
 class Stream{
     constructor(x, y, trailLen){
+        const startY = y;
         this.createStream = () => {
             for(let i = 0; i < trailLen; i++ ){
                 ctx.fillStyle = "hsla(105, 100%, 50%, "+(1 - (i/trailLen))+")";
@@ -20,15 +21,29 @@ class Stream{
             }
         }
 
+        this.moveStream = () =>{
+            if(y - (trailLen*20) > window.innerHeight){
+                y = startY;
+            }
+            y+=10;
+            this.createStream();
+        }
+
     }
 }
 
 for(let i = 0; i < noOfStreams; i++){
-    const y = Math.random()*(window.innerHeight);
+    const y = Math.random()*(-window.innerHeight);
     const x = (i+0.5) * (window.innerWidth/noOfStreams);
     streams.push( new Stream(x,y,trailLen) );
 }
 
-for(let i = 0; i < noOfStreams; i++){
-    streams[i].createStream();
+const digitalRainFall = () => {
+    requestAnimationFrame(digitalRainFall);
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+    for(let i = 0; i < noOfStreams; i++){
+        streams[i].moveStream();
+    }
 }
+
+digitalRainFall();
