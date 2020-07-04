dd149d5710a871e155bf4f03b436984609c96923
Nikhil-KNOLSKAPE <anandnikhil91@gmail.com>
2020-07-04 21:40:16 +0530

    added multiple stream (static)

--- DigitalRain/digitalRain.js
+++ DigitalRain/digitalRain.js
@@ -3,14 +3,32 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 
-const noOfStreams = 1;
-const noOfDrops = 20;
+const noOfStreams = 100;
+const trailLen = 10;
 const streams = [];
 
 ctx.font = "1vw Arial";
 ctx.textAlign = "center";
-for(let i = 0; i < noOfDrops; i++ ){
-    ctx.fillStyle = "hsla(105, 100%, 50%, "+i/noOfDrops+")";
-    ctx.fillText("0", canvas.width/2, (i*20) + canvas.height/2);  
-}
+
  
+class Stream{
+    constructor(x, y, trailLen){
+        this.createStream = () => {
+            for(let i = 0; i < trailLen; i++ ){
+                ctx.fillStyle = "hsla(105, 100%, 50%, "+(1 - (i/trailLen))+")";
+                ctx.fillText("0", x, y - (i*20) );
+            }
+        }
+
+    }
+}
+
+for(let i = 0; i < noOfStreams; i++){
+    const y = Math.random()*(window.innerHeight);
+    const x = (i+0.5) * (window.innerWidth/noOfStreams);
+    streams.push( new Stream(x,y,trailLen) );
+}
+
+for(let i = 0; i < noOfStreams; i++){
+    streams[i].createStream();
+}
