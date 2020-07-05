b4dc339481acbc633105e9c67fe9bdb58cd8e599
Nikhil-KNOLSKAPE <anandnikhil91@gmail.com>
2020-07-05 19:31:34 +0530

    added relative fontsize for different resolutions

--- DigitalRain/digitalRain.js
+++ DigitalRain/digitalRain.js
@@ -6,26 +6,30 @@ const ctx = canvas.getContext("2d");
 const noOfStreams = 150;
 const trailLen = 10;
 const streams = [];
+const fontSize = window.innerWidth/100;
+const fontStyle = String(fontSize)+"px Arial";
 
-ctx.font = "1vw Arial";
+ctx.font = fontStyle;
 ctx.textAlign = "center";
 
  
 class Stream{
-    constructor(x, y, trailLen){
+    constructor(x, y, trailLen, startLetter){
         const startY = y;
         this.createStream = () => {
             for(let i = 0; i < trailLen; i++ ){
                 ctx.fillStyle = "hsla(105, 100%, 50%, "+(1 - (i/trailLen))+")";
-                ctx.fillText("0", x, y - (i*20) );
+                ctx.fillText(Math.floor(Math.random()*10), x, y - (i*fontSize) );
             }
+            ctx.fillStyle = "hsla(105, 100%, 80%, 1)";
+            ctx.fillText(startLetter, x, y+fontSize );
         }
 
         this.moveStream = () =>{
-            if(y - (trailLen*20) > window.innerHeight){
+            if(y - (trailLen*fontSize) > window.innerHeight){
                 y = startY;
             }
-            y+=10;
+            y+=3;
             this.createStream();
         }
 
@@ -33,9 +37,10 @@ class Stream{
 }
 
 for(let i = 0; i < noOfStreams; i++){
-    const y = Math.random()*(-window.innerHeight);
+    const y = Math.random()*(-window.innerHeight - 200);
     const x = (i+0.5) * (window.innerWidth/noOfStreams);
-    streams.push( new Stream(x,y,trailLen) );
+    const startLetter = Math.floor(Math.random()*10);
+    streams.push( new Stream(x,y,trailLen,startLetter) );
 }
 
 const digitalRainFall = () => {
