ee48a1a3a77c5cf8bbced51f88013600db7fb1c8
yankovskyandrey <YankovskyAndrey@gmail.com>
2020-10-05 00:23:56 +0300

    Add an electricity like animation working based on user mouse movement

--- index.js
+++ index.js
@@ -8,18 +8,21 @@ app.get('/', (req, res) => res.sendFile( __dirname + '/index.html'));
 //*****Fractal Tree*****
 app.get('/fractalTree.js', (req, res) => res.sendFile( __dirname + '/FractalTree/fractalTree.js'));
 app.get('/Fractal-Tree', (req, res) => res.sendFile( __dirname + '/FractalTree/fractalTree.html'));
-//*****Animated Fractal Tree***** 
+//*****Animated Fractal Tree*****
 app.get('/animatedFractalTree.js', (req, res) => res.sendFile( __dirname + '/AnimatedFractalTree/animatedFractalTree.js'));
 app.get('/Animated-Fractal-Tree', (req, res) => res.sendFile( __dirname + '/AnimatedFractalTree/animatedFractalTree.html'));
-//*****Bounce***** 
+//*****Bounce*****
 app.get('/bounce.js', (req, res) => res.sendFile( __dirname + '/Bounce/bounce.js'));
 app.get('/Bounce', (req, res) => res.sendFile( __dirname + '/Bounce/bounce.html'));
-//*****Bounce Gravity***** 
+//*****Bounce Gravity*****
 app.get('/bounceGravity.js', (req, res) => res.sendFile( __dirname + '/BounceGravity/bounceGravity.js'));
 app.get('/Bounce-Gravity', (req, res) => res.sendFile( __dirname + '/BounceGravity/bounceGravity.html'));
-//*****Digital Rain***** 
+//*****Digital Rain*****
 app.get('/digitalRain.js', (req, res) => res.sendFile( __dirname + '/DigitalRain/digitalRain.js'));
 app.get('/Digital-Rain', (req, res) => res.sendFile( __dirname + '/DigitalRain/digitalRain.html'));
+//*****Mouse Move*****
+app.get('/mouseMove.js', (req, res) => res.sendFile( __dirname + '/MouseMove/mouseMove.js'));
+app.get('/Mouse-Move', (req, res) => res.sendFile( __dirname + '/MouseMove/mouseMove.html'));
 
 app.listen(PORT, function(req, res) {
     console.log('Server running');
